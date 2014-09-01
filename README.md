832671ea1cff56b7aa6bc183fdbedffd004bcb37
George Bryant <gb@gbryant.co.uk>
2014-09-01 21:51:57 +0100

    Add pseudocode .py files and dependencies to README
    amazon-wishlist-tools.py
        Main .py file - imports others and runs them
        Print output to terminal
    
    arg_parser.py
        Arguments are defined in this file
    
    get_wishlist.py
        Use beautifulsoup to scrape the data from the wishlist page

--- README.md
+++ README.md
@@ -4,3 +4,9 @@ Useful tools for your Amazon wishlist
 -------------------------------------
 
 Default behaviour will be to show a total for the wishlist along with a list of the items
+
+Dependencies
+------------
+
+* beautifulsoup
+* prettytable
---
+++ src/get_wishlist.py
@@ -0,0 +1,41 @@
+"""
+Amazon-wishlist-tools - Useful tools for your Amazon wishlist
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+"""
+
+import beautifulsoup as soup
+
+"""
+def get(args):
+    url = construct_url(args)
+    page = retrieve(url)
+    wishlist = parse_page(page)
+    return wishlist
+
+def construct_url(args):
+    Add necessary arguments onto url
+    http://www.artiss.co.uk/2012/10/how-to-sequence-a-shared-amazon-wish-list
+    return url
+
+def retrieve(url):
+    Get page from amazon
+    return page_html
+
+def parse_page(page):
+    Use beautifulsoup to get necessary parts of page:
+        Name, price, num(wanted-got), priority
+    Make these into list of dicts - each dict being an item on the list
+    return wishlist_dict
+
