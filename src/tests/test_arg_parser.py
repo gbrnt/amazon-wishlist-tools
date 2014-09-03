b779115f384726ed77078c2f9313af6b6e3a7630
George Bryant <gb@gbryant.co.uk>
2014-09-03 20:29:27 +0100

    Move tests to src/tests directory

---
+++ src/tests/test_arg_parser.py
@@ -0,0 +1,46 @@
+"""
+Tests for amazon-wishlist-tools - Useful tools for your Amazon wishlist
+"""
+
+import unittest
+import argparse
+import arg_parser
+
+
+class ParseArguments(unittest.TestCase):
+    def test_create_parser(self):
+        self.parser = arg_parser.create_parser()
+
+        self.assertIsInstance(parser, argparse.ArgumentParser)
+
+    def test_parser_creates_namespace(self):
+        args = self.parser.parse_args(["http://amazon.co.uk"])
+        
+        self.assertIsInstance(args, Namespace)
+
+    def test_parser_defaults(self):
+        args = self.parser.parse_args(["http://amazon.co.uk"])
+
+        self.assertEqual(args.reveal, "all")
+        self.assertEqual(args.sort, "last-updated")
+        self.asertEqual(args.total, "False")
+        self.assertEqual(args.url, "http://amazon.co.uk")
+
+    def test_parser_exception_on_no_url(self):
+        """Argument parser should raise an exception [name to be found] when no URL is given"""
+        self.assertRaises(ExceptionName, self.parser.parse_args)
+
+    def test_parser_given_all_args(self):
+        args_to_give = [
+            "--reveal", "unpurchased",
+            "--sort", "priority",
+            "--total",
+            "http://amazon.co.uk"
+        ]
+
+        args = self.parser.parse(args(args_to_give))
+
+        self.assertEqual(args.reveal, "unpurchased")
+        self.assertEqual(args.sort, "priority")
+        self.assertEqual(args.total, True)
+        self.assertEqual(args.url, "http://amazon.co.uk")
